e71e8e2cbb44aa643ab253c2c22418ff0ca5af67
Hiteshwar <hiteshwar502133p@gmail.com>
2026-04-01 14:21:36 +0530

    Fix swagger route

--- expense_analyzer/urls.py
+++ expense_analyzer/urls.py
@@ -6,7 +6,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger setup
 schema_view = get_schema_view(
     openapi.Info(
         title="Expense Analyzer API",
@@ -25,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 
-    # 🔥 Swagger
+    # 🔥 MUST BE HERE
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
