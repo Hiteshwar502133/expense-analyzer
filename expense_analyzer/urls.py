b76c21cd4c3782a84671e179b34e203638c021d1
Hiteshwar <hiteshwar502133p@gmail.com>
2026-04-01 13:50:36 +0530

    Added homepage route

--- expense_analyzer/urls.py
+++ expense_analyzer/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# 👇 add this function
+def home(request):
+    return HttpResponse("Expense Analyzer API is running 🚀")
 
 urlpatterns = [
+    path('', home),  # 👈 THIS LINE ADDED
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # IMPORTANT
+    path('api/', include('api.urls')),  # keep your existing one
 ]
